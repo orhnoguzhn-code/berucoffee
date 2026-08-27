e1cf71f7c53cba03a287b09689e0552e426884ec
orhnoguzhn-code <orhnoguzhn@gmail.com>
2026-08-27 09:15:17 +0300

    Uygulama hata sınırını ve tema durum çubuğunu uygula

--- App.tsx
+++ App.tsx
@@ -7,22 +7,26 @@ import { I18nProvider } from './src/i18n/I18nContext';
 import { CartProvider } from './src/context/CartContext';
 import AppNavigator from './src/navigation/AppNavigator';
 import NotificationHandler from './src/components/NotificationHandler';
+import AppErrorBoundary from './src/components/AppErrorBoundary';
+import { colors } from './src/theme';
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <KeyboardProvider>
-        <I18nProvider>
-          <AuthProvider>
-            <CartProvider>
-              <NotificationHandler>
-                <AppNavigator />
-              </NotificationHandler>
-              <StatusBar barStyle="dark-content" backgroundColor="#F8F4F0" />
-            </CartProvider>
-          </AuthProvider>
-        </I18nProvider>
-      </KeyboardProvider>
-    </GestureHandlerRootView>
+    <AppErrorBoundary>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <KeyboardProvider>
+          <I18nProvider>
+            <AuthProvider>
+              <CartProvider>
+                <NotificationHandler>
+                  <AppNavigator />
+                </NotificationHandler>
+                <StatusBar barStyle="dark-content" backgroundColor={colors.bgSoft} />
+              </CartProvider>
+            </AuthProvider>
+          </I18nProvider>
+        </KeyboardProvider>
+      </GestureHandlerRootView>
+    </AppErrorBoundary>
   );
 }
